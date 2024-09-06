61c19df3969e6c22e57a7476a17639abc1b5cab6
GABRIEL PRADEGAN ORSATTO <1135097@ATITUS.LOCAL>
2024-09-05 22:03:59 -0300

    Commit 2

--- saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoController.java
+++ saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoController.java
@@ -2,13 +2,24 @@ package br.edu.atitus.paradigma.saudacao_service;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/saudacao-service")
 public class SaudacaoController {
 
-	@GetMapping
-	public ResponseEntity<String> getSaudacao() {
-		return ResponseEntity.ok("Hello World!");
+	@GetMapping({"", "/", "/{nomePath}"})
+	public ResponseEntity<String> getSaudacao(
+			@RequestParam(required = false) String nome,
+			@PathVariable(required = false) String nomePath) {
+
+		if(nome == null) 
+			nome = nomePath == null ? "World" : nomePath;
+		
+		String template = "%s %s!!!";
+		return ResponseEntity.ok(String.format(template, "Hello", nome));
 	}
 }
